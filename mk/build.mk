4dc8ac29e363e9a19bf9ed4c36079ffbd8de439f
Tom Everett <tom@khubla.com>
2020-12-12 19:09:49 -0700

    LDFLAGS in build.mk

--- mk/build.mk
+++ mk/build.mk
@@ -1,8 +1,17 @@
 
 NASM=nasm -f bin
 RM=rm -f
-CC=gcc
-AR=ar
 MAKE=make
+
+# cc
+CC=gcc
+
+# ar
+AR=ar
+
+# ld
 LD=ld
+#LDFLAGS=-e CosmOS --oformat binary -Ttext 0xFFFF800000000000 -nostdlib
+LDFLAGS=-T cosmos.ld -Map cosmos.map
+
 
