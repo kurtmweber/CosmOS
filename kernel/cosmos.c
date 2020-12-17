919f2b1c248dbe298d1b7057de6da38566aef2f2
Tom Everett <tom@khubla.com>
2020-12-17 10:59:03 -0700

    less output

--- kernel/cosmos.c
+++ kernel/cosmos.c
@@ -92,7 +92,7 @@ void CosmOS(){
 	tmp = usable_phys_blocks;
 
 	do {
-		kprintf("Base: %llX, Length: %llX\n", (uint64_t)tmp->base, tmp->len);
+//		kprintf("Base: %llX, Length: %llX\n", (uint64_t)tmp->base, tmp->len);
 	} while((tmp = tmp->next));
 
 	phys_alloc_slab(65536, 65536);
@@ -100,7 +100,7 @@ void CosmOS(){
 	tmp = usable_phys_blocks;
 
 	do {
-		kprintf("Base: %llX, Length: %llX\n", (uint64_t)tmp->base, tmp->len);
+//		kprintf("Base: %llX, Length: %llX\n", (uint64_t)tmp->base, tmp->len);
 	} while((tmp = tmp->next));
 	
 	while (1){
