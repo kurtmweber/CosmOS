6e2ce804a978599eb05296f7a5261b14e1f3f3f7
Tom Everett <tom@khubla.com>
2020-12-17 09:31:30 -0700

    ifdef fixes

--- kernel/console/drivers/vga/vga.h
+++ kernel/console/drivers/vga/vga.h
@@ -8,24 +8,18 @@
 #ifndef _VGA_CONSOLE_H
 #define _VGA_CONSOLE_H
 
+#include <types.h>
+
 #define VGA_CONSOLE_ENABLED
 
-#ifndef _VGA_CONSOLE_C
 extern uint16_t vga_console_x_width;
 extern uint16_t vga_console_y_height;
 extern uint8_t vga_console_xpos;
 extern uint8_t vga_console_ypos;
 
 void vga_console_register();
-#else
-uint16_t vga_console_x_width;
-uint16_t vga_console_y_height;
-uint8_t vga_console_xpos;
-uint8_t vga_console_ypos;
-
 void vga_console_init();
 uint8_t vga_console_setpos(uint8_t x, uint8_t y);
 uint8_t vga_console_write(const char *c);
-#endif
 
 #endif
