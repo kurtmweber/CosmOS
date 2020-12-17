b3355d65ff9e720bf62b4c1e48fbf1cf413a257e
Tom Everett <tom@khubla.com>
2020-12-17 10:41:29 -0700

    PCI registration

--- kernel/network/rtl8139/rtl8139.c
+++ kernel/network/rtl8139/rtl8139.c
@@ -19,30 +19,25 @@ void rtl8139_irq_handler(){
 /*
 * perform device instance specific init here
 */
-void deviceInitRTL8139(struct device* dev){
+void RTL8139Init(struct device* dev){
     struct pci_device* pci_dev = (struct pci_device*) dev->deviceData;
     registerInterruptHandler(pci_dev->irq, &rtl8139_irq_handler);
     kprintf("Init RTL8139 at IRQ %llu\n", pci_dev->irq);
+}
+
+void RTL8139SearchCB(struct pci_device* dev){
+    /*
+    * register device
+    */
+    struct device* deviceinstance = newDevice();
+    deviceinstance->init =  &RTL8139Init;
+    deviceinstance->deviceData = dev;
+    registerDevice(deviceinstance);
 }
 
 /**
 * find all RTL8139 devices and register them
 */
 void rtl8139_register_devices() {
-    uint16_t i = 0;
-    for (i = 0; i < num_pci_devices; i++){
-        if ((pci_devices[i].pci_class == PCI_CLASS_NETWORK) && ((pci_network_subclass_codes)pci_devices[i].pci_subclass == PCI_NETWORK_SUBCLASS_ETHERNET)) {
-            if ((pci_devices[i].vendor_id==0x10EC) && (pci_devices[i].device_id==0x8139)) {
-                kprintf("RTL 8139 Network Controller found at PCI address %#hX:%#hX:%#hX\n", pci_devices[i].bus, pci_devices[i].device, pci_devices[i].function);
-                kprintf("\tVendor %#X, Device %#X\n", pci_devices[i].vendor_id, pci_devices[i].device_id);
-                /*
-                * register device
-                */
-                struct device* deviceinstance = newDevice();
-                deviceinstance->init =  &deviceInitRTL8139;
-                deviceinstance->deviceData = &(pci_devices[i]);
-                registerDevice(deviceinstance);
-            }
-        }
-    }
+    pci_search(PCI_CLASS_NETWORK,PCI_NETWORK_SUBCLASS_ETHERNET,0x10EC,0x8139, &RTL8139SearchCB);
 }
--- kernel/pci/pci_device.c
+++ kernel/pci/pci_device.c
@@ -7,6 +7,7 @@
 
 #include <pci/pci_device.h>
 #include <types.h>
+#include <console/console.h>
 
 struct pci_device *pci_devices;
 uint16_t num_pci_devices;
@@ -14,8 +15,10 @@ uint16_t num_pci_devices;
 void pci_search(pci_class_codes pci_class, uint8_t pci_subclass, uint16_t vendor_id, uint16_t device_id, deviceSearchCallback cb){
     uint16_t i = 0;
     for (i = 0; i < num_pci_devices; i++){
-        if ((pci_devices[i].pci_class == pci_class) && ((pci_network_subclass_codes)pci_devices[i].pci_subclass == pci_subclass)) {
+        if ((pci_devices[i].pci_class == pci_class) && (pci_devices[i].pci_subclass == pci_subclass)) {
             if ((pci_devices[i].vendor_id==vendor_id) && (pci_devices[i].device_id==device_id)) {
+                kprintf("PCI Device found at PCI address %#hX:%#hX:%#hX\n", pci_devices[i].bus, pci_devices[i].device, pci_devices[i].function);
+                kprintf("\tVendor %#X, Device %#X\n", pci_devices[i].vendor_id, pci_devices[i].device_id);
                 (*cb)(&(pci_devices[i]));
             }
         }
