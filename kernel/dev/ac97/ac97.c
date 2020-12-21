f4712e4092911af3ac78209e154770bb7821ae61
Tom Everett <tom@khubla.com>
2020-12-21 10:38:05 -0700

    ac97 parameters

--- kernel/dev/ac97/ac97.c
+++ kernel/dev/ac97/ac97.c
@@ -13,6 +13,28 @@
 #include <interrupts/interrupt_router.h>
 #include <dev/pci/pci.h>
 
+// https://wiki.osdev.org/AC97
+
+#define AC97_RESET                 0x00
+#define AC97_MASTER_VOLUME         0x02
+#define AC97_AUX_OUT_VOLUME        0x04
+#define AC97_MONO_VOLUME           0x06
+#define AC97_MASTER_TONE           0x08
+
+#define AC97_PC_BEEP_VOLUME        0x0A
+#define AC97_PHONE_VOLUME          0x0C
+#define AC97_MIC_VOLUME            0x0E
+#define AC97_LINEIN_VOLUME         0x10
+#define AC97_CD_VOLUME             0x12
+#define AC97_VIDEO_VOLUME          0x14
+#define AC97_AUXIN_VOLUME          0x16
+#define AC97_PCMOUT_VOLUME         0x18
+#define AC97_RECORD_SELECT         0x1A
+#define AC97_RECORD_GAIN           0x1C
+#define AC97_RECORD_GAIN_MIC       0x1E
+#define AC97_GENERAL_PURPOSE       0x20
+#define AC97_3D_CONTROL            0x22
+
 void ac97_handle_irq(stackFrame *frame) {
 }
 
