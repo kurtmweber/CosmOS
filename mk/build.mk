503397f34e70071fcc3cbf1eed4d2928a8bfc6ab
Tom Everett <tom@khubla.com>
2020-12-22 13:53:33 -0700

    ehci controller

--- mk/build.mk
+++ mk/build.mk
@@ -35,7 +35,6 @@ QEMUARGS=                                                 \
   -no-reboot                                              \
   -drive format=raw,file=hda.img                          \
   -smp 1                                                  \
-  -usb                                                    \
   -nic user,ipv6=off,model=ne2k_isa,mac=52:54:98:76:54:32 \
   -serial stdio                                           \
   -machine pcspk-audiodev=audio0                          \
@@ -43,7 +42,7 @@ QEMUARGS=                                                 \
   -monitor telnet::45454,server,nowait                    \
   -device floppy,drive=f0                                 \
   -blockdev driver=file,node-name=f0,filename=hda.img     \
-  -device usb-mouse                                       \
+  -device usb-ehci                                       \
 
 # note that we're mounting the hda.img as a floppy image. good enough for now.
 # the first bytes are FA B4 00 B0 03 CD 10 88 16 7C 7C B8 01 24 CD 15
