17e2a00de918d0d8c3636d029b8e6a4d3a43063b
Tom Everett <tom@khubla.com>
2020-12-22 19:16:58 -0700

    virtio fun

--- kernel/dev/virtio/vnic/vnic.c
+++ kernel/dev/virtio/vnic/vnic.c
@@ -7,7 +7,7 @@
 
 // https://wiki.osdev.org/Virtio*/
 
-#include <dev/network/vnic/vnic.h>
+#include <dev/virtio/vnic/vnic.h>
 #include <interrupts/interrupt_router.h>
 #include <asm/asm.h>
 #include <devicemgr/devicemgr.h>
@@ -17,7 +17,7 @@
 #include <sleep/sleep.h>
 #include <dev/pci/pci.h>
 
-#include <dev/network/vnic/virtio.h>
+#include <dev/virtio/virtio.h>
 
 // virtio NIC flags
 #define VIRTIO_NIC_MAC1             0x14
@@ -36,6 +36,29 @@
 #define VIRTIO_BLOCK_HEAD_COUNT         0x26
 #define VIRTIO_BLOCK_SECTOR_COUNT       0x27
 #define VIRTIO_BLOCK_LENGTH             0x28
+
+struct VirtioPacketHeader {
+  uint8_t Flags;                // Bit 0: Needs checksum; Bit 1: Received packet has valid data;
+                                // Bit 2: If VIRTIO_NET_F_RSC_EXT was negotiated, the device processes
+                                // duplicated ACK segments, reports number of coalesced TCP segments in ChecksumStart
+                                // field and number of duplicated ACK segments in ChecksumOffset field,
+                                // and sets bit 2 in Flags(VIRTIO_NET_HDR_F_RSC_INFO) 
+  uint8_t SegmentationOffload;  // 0:None 1:TCPv4 3:UDP 4:TCPv6 0x80:ECN
+  uint16_t HeaderLength;        // Size of header to be used during segmentation.
+  uint16_t SegmentLength;       // Maximum segment size (not including header).
+  uint16_t ChecksumStart;       // The position to begin calculating the checksum.
+  uint16_t ChecksumOffset;      // The position after ChecksumStart to store the checksum.
+  uint16_t BufferCount;         // Used when merging buffers.
+};
+
+// TODO move this into virtio block device
+struct BlockRequest {
+  uint32_t Type;              // 0: Read; 1: Write; 4: Flush; 11: Discard; 13: Write zeroes
+  uint32_t Reserved;
+  uint64_t Sector;
+  uint8_t Data[0];             // Data's size must be a multiple of 512
+  uint8_t Status;             // 0: OK; 1: Error; 2: Unsupported
+};
 
 void vnic_irq_handler(stackFrame *frame){
 	kprintf("#");
