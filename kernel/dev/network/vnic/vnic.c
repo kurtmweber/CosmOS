cbfb2b8f3a75bdafc766bfddd776b0387b33eaa7
Tom Everett <tom@khubla.com>
2020-12-22 18:50:50 -0700

    virtio defines

--- kernel/dev/network/vnic/vnic.c
+++ kernel/dev/network/vnic/vnic.c
@@ -17,6 +17,33 @@
 #include <sleep/sleep.h>
 #include <dev/pci/pci.h>
 
+// generic virtio flags
+#define VIRTIO_DEVICE_FEATURES      0x00
+#define VIRTIO_GUEST_FEATURES       0x04
+#define VIRTIO_QUEUE_ADDRESS        0x08
+#define VIRTIO_QUEUE_SIZE           0x0C
+#define VIRTIO_QUEUE_SELECT         0x0E
+#define VIRTIO_QUEUE_NOTIFY         0x10
+#define VIRTIO_DEVICE_STATUS        0x12
+#define VIRTIO_ISR_STATUS           0x13
+
+// virtio NIC flags
+#define VIRTIO_NIC_MAC1             0x14
+#define VIRTIO_NIC_MAC2             0x15
+#define VIRTIO_NIC_MAC3             0x16
+#define VIRTIO_NIC_MAC4             0x17
+#define VIRTIO_NIC_MAC5             0x18
+#define VIRTIO_NIC_MAC6             0x19
+#define VIRTIO_NIC_STATUS           0x1A
+
+// cut and paste these later into virtio block device
+#define VIRTIO_BLOCK_TOTAL_SECTORS      0x14
+#define VIRTIO_BLOCK_MAX_SEGMENT_SIZE   0x1C
+#define VIRTIO_BLOCK_MAX_SEGMENT_COUNT  0x20
+#define VIRTIO_BLOCK_CYLINDER_COUNT     0x24
+#define VIRTIO_BLOCK_HEAD_COUNT         0x26
+#define VIRTIO_BLOCK_SECTOR_COUNT       0x27
+#define VIRTIO_BLOCK_LENGTH             0x28
 
 void vnic_irq_handler(stackFrame *frame){
 	kprintf("#");
@@ -27,7 +54,11 @@ void vnic_irq_handler(stackFrame *frame){
 void VNICInit(struct device* dev){
     struct pci_device* pci_dev = (struct pci_device*) dev->deviceData;
     interrupt_router_register_interrupt_handler(pci_dev->irq, &vnic_irq_handler);
-    kprintf("Init %s at IRQ %llu Vendor %#hX Device %#hX (%s)\n",dev->description, pci_dev->irq,pci_dev->vendor_id, pci_dev->device_id, dev->name);
+
+    // TODO. There is stuff to merge and when that happens, bar0 will be in the pci dev struct
+    uint64_t base = pci_header_read_bar0(pci_dev->bus, pci_dev->device,pci_dev->function);
+
+    kprintf("Init %s at IRQ %llu Vendor %#hX Device %#hX Base %#hX (%s)\n",dev->description, pci_dev->irq,pci_dev->vendor_id, pci_dev->device_id, base, dev->name);
 }
 
 void VNICSearchCB(struct pci_device* dev){
--- kernel/dev/network/vnic/vnic.h
+++ kernel/dev/network/vnic/vnic.h
@@ -10,6 +10,6 @@
 
 #include <types.h>
 
-void vinc_devicemgr_register_devices();
+void vnic_devicemgr_register_devices();
 
 #endif
