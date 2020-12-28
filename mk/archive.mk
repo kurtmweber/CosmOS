25ebd239e1e6072d6d46d7cc12d6a6c7842e866c
Tom Everett <tom@khubla.com>
2020-12-27 21:10:14 -0700

    whups

--- mk/archive.mk
+++ mk/archive.mk
@@ -1,3 +1,10 @@
+#*****************************************************************
+# This file is part of CosmOS                                    *
+# Copyright (C) 2020 Tom Everett                                 *
+# Released under the stated terms in the file LICENSE            *
+# See the file "LICENSE" in the source distribution for details  *
+# ****************************************************************
+
 ## generic instructions for building an archive
 ## everything below this point is the same for every archive.....
 
