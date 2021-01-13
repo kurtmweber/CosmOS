a08da6485baadf722d6fb6de3e3d0352822fbee7
Tom Everett <tom@khubla.com>
2021-01-13 14:20:11 -0700

    code cleanup

--- kernel/dev/i386/ac97/ac97.c
+++ kernel/dev/i386/ac97/ac97.c
@@ -6,84 +6,83 @@
  *****************************************************************/
 
 #include <dev/i386/ac97/ac97.h>
-#include <sys/asm/asm.h>
-#include <sys/devicemgr/devicemgr.h>
-#include <sys/console/console.h>
-#include <sys/collection/list/list.h>
-#include <sys/interrupt_router/interrupt_router.h>
 #include <dev/i386/pci/pci.h>
-#include <sys/deviceapi/deviceapi_dsp.h>
+#include <sys/asm/asm.h>
+#include <sys/collection/list/list.h>
+#include <sys/console/console.h>
 #include <sys/debug/assert.h>
+#include <sys/deviceapi/deviceapi_dsp.h>
+#include <sys/devicemgr/devicemgr.h>
+#include <sys/interrupt_router/interrupt_router.h>
 
 // https://wiki.osdev.org/AC97
 
-#define AC97_RESET                 0x00
-#define AC97_MASTER_VOLUME         0x02
-#define AC97_AUX_OUT_VOLUME        0x04
-#define AC97_MONO_VOLUME           0x06
-#define AC97_MASTER_TONE           0x08
+#define AC97_RESET 0x00
+#define AC97_MASTER_VOLUME 0x02
+#define AC97_AUX_OUT_VOLUME 0x04
+#define AC97_MONO_VOLUME 0x06
+#define AC97_MASTER_TONE 0x08
 
-#define AC97_PC_BEEP_VOLUME        0x0A
-#define AC97_PHONE_VOLUME          0x0C
-#define AC97_MIC_VOLUME            0x0E
-#define AC97_LINEIN_VOLUME         0x10
-#define AC97_CD_VOLUME             0x12
-#define AC97_VIDEO_VOLUME          0x14
-#define AC97_AUXIN_VOLUME          0x16
-#define AC97_PCMOUT_VOLUME         0x18
-#define AC97_RECORD_SELECT         0x1A
-#define AC97_RECORD_GAIN           0x1C
-#define AC97_RECORD_GAIN_MIC       0x1E
-#define AC97_GENERAL_PURPOSE       0x20
-#define AC97_3D_CONTROL            0x22
+#define AC97_PC_BEEP_VOLUME 0x0A
+#define AC97_PHONE_VOLUME 0x0C
+#define AC97_MIC_VOLUME 0x0E
+#define AC97_LINEIN_VOLUME 0x10
+#define AC97_CD_VOLUME 0x12
+#define AC97_VIDEO_VOLUME 0x14
+#define AC97_AUXIN_VOLUME 0x16
+#define AC97_PCMOUT_VOLUME 0x18
+#define AC97_RECORD_SELECT 0x1A
+#define AC97_RECORD_GAIN 0x1C
+#define AC97_RECORD_GAIN_MIC 0x1E
+#define AC97_GENERAL_PURPOSE 0x20
+#define AC97_3D_CONTROL 0x22
 
 struct ac97_devicedata {
     uint64_t base;
 } __attribute__((packed));
 
-void ac97_handle_irq(stackFrame *frame) {
-	ASSERT_NOT_NULL(frame);
+void ac97_handle_irq(stackFrame* frame) {
+    ASSERT_NOT_NULL(frame);
 }
 
 /*
-* perform device instance specific init here
-*/
-void deviceInitAC97(struct device* dev){
-	ASSERT_NOT_NULL(dev);
-    struct ac97_devicedata* deviceData = (struct ac97_devicedata*) dev->deviceData;
+ * perform device instance specific init here
+ */
+void deviceInitAC97(struct device* dev) {
+    ASSERT_NOT_NULL(dev);
+    struct ac97_devicedata* deviceData = (struct ac97_devicedata*)dev->deviceData;
     deviceData->base = pci_calcbar(dev->pci);
-    kprintf("Init %s at IRQ %llu Vendor %#hX Device %#hX Base %#hX (%s)\n",dev->description, dev->pci->irq,dev->pci->vendor_id, dev->pci->device_id, deviceData->base, dev->name);
+    kprintf("Init %s at IRQ %llu Vendor %#hX Device %#hX Base %#hX (%s)\n", dev->description, dev->pci->irq, dev->pci->vendor_id, dev->pci->device_id, deviceData->base, dev->name);
     interrupt_router_register_interrupt_handler(dev->pci->irq, &ac97_handle_irq);
 }
 
-void AC97PCISearchCB(struct pci_device* dev){
-	ASSERT_NOT_NULL(dev);
+void AC97PCISearchCB(struct pci_device* dev) {
+    ASSERT_NOT_NULL(dev);
     /*
-    * register device
-    */
+     * register device
+     */
     struct device* deviceinstance = devicemgr_new_device();
-    deviceinstance->init =  &deviceInitAC97;
+    deviceinstance->init = &deviceInitAC97;
     deviceinstance->pci = dev;
     deviceinstance->devicetype = DSP;
     devicemgr_set_device_description(deviceinstance, "Intel 82801AA AC97");
-	/*
-	* device api
-	*/
-	struct deviceapi_dsp* api = (struct deviceapi_dsp*) kmalloc (sizeof(struct deviceapi_dsp));
-	deviceinstance->api = api;
     /*
-    * the deviceData
-    */
-    struct ac97_devicedata* deviceData = (struct ac97_devicedata*) kmalloc(sizeof(struct ac97_devicedata));
+     * device api
+     */
+    struct deviceapi_dsp* api = (struct deviceapi_dsp*)kmalloc(sizeof(struct deviceapi_dsp));
+    deviceinstance->api = api;
+    /*
+     * the deviceData
+     */
+    struct ac97_devicedata* deviceData = (struct ac97_devicedata*)kmalloc(sizeof(struct ac97_devicedata));
     deviceData->base = 0;
     deviceinstance->deviceData = deviceData;
-	/*
-	* register
-	*/
-	devicemgr_register_device(deviceinstance);
+    /*
+     * register
+     */
+    devicemgr_register_device(deviceinstance);
 }
 
-void ac97_devicemgr_register_devices(){
-	pci_devicemgr_search_device(PCI_CLASS_MULTIMEDIA,0x01, 0x8086,0x2415, &AC97PCISearchCB);
+void ac97_devicemgr_register_devices() {
+    pci_devicemgr_search_device(PCI_CLASS_MULTIMEDIA, 0x01, 0x8086, 0x2415, &AC97PCISearchCB);
 }
-
--- kernel/dev/i386/adlib/adlib.c
+++ kernel/dev/i386/adlib/adlib.c
@@ -7,42 +7,41 @@
 
 #include <dev/i386/adlib/adlib.h>
 #include <sys/asm/asm.h>
-#include <sys/devicemgr/devicemgr.h>
-#include <sys/console/console.h>
 #include <sys/collection/list/list.h>
-#include <sys/interrupt_router/interrupt_router.h>
-#include <sys/deviceapi/deviceapi_dsp.h>
+#include <sys/console/console.h>
 #include <sys/debug/assert.h>
+#include <sys/deviceapi/deviceapi_dsp.h>
+#include <sys/devicemgr/devicemgr.h>
+#include <sys/interrupt_router/interrupt_router.h>
 
-void adlib_handle_irq(stackFrame *frame) {
-	ASSERT_NOT_NULL(frame);
+void adlib_handle_irq(stackFrame* frame) {
+    ASSERT_NOT_NULL(frame);
 }
 
 /*
-* perform device instance specific init here
-*/
-void adlib_device_init(struct device* dev){
-	ASSERT_NOT_NULL(dev);
- //   kprintf("Init %s at IRQ %llu\n",dev->description, SB16_IRQ);
- //   interrupt_router_register_interrupt_handler(SB16_IRQ, &adlib_handle_irq);
+ * perform device instance specific init here
+ */
+void adlib_device_init(struct device* dev) {
+    ASSERT_NOT_NULL(dev);
+    //   kprintf("Init %s at IRQ %llu\n",dev->description, SB16_IRQ);
+    //   interrupt_router_register_interrupt_handler(SB16_IRQ, &adlib_handle_irq);
 }
 
-void adlib_devicemgr_register_devices(){
+void adlib_devicemgr_register_devices() {
     /*
-	* register device
-	*/
-	struct device* deviceinstance = devicemgr_new_device();
-	devicemgr_set_device_description(deviceinstance, "Yamaha YM3812 (OPL2)");
-	deviceinstance->devicetype = DSP;
-	deviceinstance->init =  &adlib_device_init;
-	/*
-	* device api
-	*/
-	struct deviceapi_dsp* api = (struct deviceapi_dsp*) kmalloc (sizeof(struct deviceapi_dsp));
-	deviceinstance->api = api;
-	/*
-	* register
-	*/
-	devicemgr_register_device(deviceinstance);
+     * register device
+     */
+    struct device* deviceinstance = devicemgr_new_device();
+    devicemgr_set_device_description(deviceinstance, "Yamaha YM3812 (OPL2)");
+    deviceinstance->devicetype = DSP;
+    deviceinstance->init = &adlib_device_init;
+    /*
+     * device api
+     */
+    struct deviceapi_dsp* api = (struct deviceapi_dsp*)kmalloc(sizeof(struct deviceapi_dsp));
+    deviceinstance->api = api;
+    /*
+     * register
+     */
+    devicemgr_register_device(deviceinstance);
 }
-
