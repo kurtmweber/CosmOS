0d60f1c0e3cf6c20be1fb420a08d6b1dd64a2125
Tom Everett <tom@khubla.com>
2021-01-13 18:24:48 -0700

    year fix

--- kernel/dev/i386/bda/bda.h
+++ kernel/dev/i386/bda/bda.h
@@ -1,6 +1,6 @@
 //*****************************************************************
 // This file is part of CosmOS                                    *
-// Copyright (C) 2020 Tom Everett                                 *
+// Copyright (C) 2021 Tom Everett                                 *
 // Released under the stated terms in the file LICENSE            *
 // See the file "LICENSE" in the source distribution for details  *
 // ****************************************************************
