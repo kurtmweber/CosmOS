1a0f79fcf45227badd6187b0c7c98ab8016bbbd3
Tom Everett <tom@khubla.com>
2021-01-17 21:19:51 -0700

    docs

--- kernel/sys/devicemgr/devicemgr.h
+++ kernel/sys/devicemgr/devicemgr.h
@@ -21,34 +21,34 @@ typedef void (*deviceUnInit)(struct device* dev);
 
 typedef enum deviceType {
     NONE = 0x00,
-    SERIAL = 0x01,
-    VGA = 0x02,
-    RTC = 0x03,
-    KEYBOARD = 0x04,
-    NIC = 0x05,  // uses NIC API
-    BRIDGE = 0x06,
-    USB = 0x07,
-    ATA = 0x08,  // ATA controller
-    PIC = 0x09,
-    MOUSE = 0x0A,
-    FLOPPY = 0x0B,
-    SPEAKER = 0x0C,
-    PIT = 0x0D,
-    DSP = 0x0E,
-    CMOS = 0x0F,
-    DMA = 0x10,
-    CPU = 0x11,
-    RAMDISK = 0x12,  // uses Block API
-    VNIC = 0x13,     // uses NIC API
-    VBLOCK = 0x14,   // uses Block API
-    DISK = 0x15,     // uses Block API
-    PARALLEL = 0x16,
-    BDA = 0x17,
-    EBDA = 0x18,
-    SWAP = 0x19,
-    FILESYSTEM = 0x1A,
-    PARTITION_TABLE = 0x1B,
-    CONSOLE = 0x1C
+    SERIAL = 0x01,           // serial0, deviceapi_serial
+    VGA = 0x02,              // vga0, deviceapi_vga
+    RTC = 0x03,              // rtc0, deviceapi_rtc
+    KEYBOARD = 0x04,         // keyboard0, deviceapi_keyboard
+    NIC = 0x05,              // nic0, deviceapi_ethernet
+    BRIDGE = 0x06,           // bridge0, deviceapi_bridge
+    USB = 0x07,              // usb0, deviceapi_usb
+    ATA = 0x08,              // ata0
+    PIC = 0x09,              // pic0, deviceapi_pic
+    MOUSE = 0x0A,            // mouse0, deviceapi_mouse
+    FLOPPY = 0x0B,           // floppy0, deviceapi_floppy
+    SPEAKER = 0x0C,          // speaker0, deviceapi_speaker
+    PIT = 0x0D,              // pit0, deviceapi_pit
+    DSP = 0x0E,              // dsp0, deviceapi_dsp
+    CMOS = 0x0F,             // cmos0, deviceapi_cmos
+    DMA = 0x10,              // dma0
+    CPU = 0x11,              // cpu0, deviceapi_cpu
+    RAMDISK = 0x12,          // rd0, deviceapi_block
+    VNIC = 0x13,             // vnic0, deviceapi_ethernet
+    VBLOCK = 0x14,           // vblock0, deviceapi_block
+    DISK = 0x15,             // disk0, deviceapi_block
+    PARALLEL = 0x16,         // parallel0, deviceapi_parallel
+    BDA = 0x17,              // bda0
+    EBDA = 0x18,             // ebda0
+    SWAP = 0x19,             // swap0, deviceapi_swap
+    FILESYSTEM = 0x1A,       // fs0, deviceapi_filesytem
+    PARTITION_TABLE = 0x1B,  // pt0, deviceapi_part_table
+    CONSOLE = 0x1C           // console0, deviceapi_console
 } deviceType;
 
 /*
