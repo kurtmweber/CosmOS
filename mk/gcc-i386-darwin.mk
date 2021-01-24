daf647728284c5d55ff875ba5407404acfd8af79
Tom Everett <tom@khubla.com>
2021-01-24 16:45:29 -0700

    space

--- mk/gcc-i386-darwin.mk
+++ mk/gcc-i386-darwin.mk
@@ -14,7 +14,7 @@ CC=x86_64-elf-gcc
 CFLAGS=-c -m64 -mno-red-zone -ffreestanding -fPIC -O0 -D$(COMPILE_PLATFORM) -D$(TARGET_PLATFORM)
 
 # ld
-LD=x86_64-elf-ld 
+LD=x86_64-elf-ld
 LDFLAGS=-m elf_x86_64 -T $(LINKER_SCRIPT) -Map $(MAPFILE) -nostdlib --no-relax
 
 # objcopy
